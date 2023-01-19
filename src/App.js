e0d2eb9d0db10b7e086c1f64ba855491d3501e6b
Nitsorn <nitsorn.oat@gmail.com>
2023-01-19 18:09:50 -0500

    some content

--- src/App.js
+++ src/App.js
@@ -5,20 +5,26 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>logo</div>
+        <div>Some links</div>
       </header>
-    </div>
+      <div className='App-body'>
+        <div className='App-Content'>
+          <div className='Definition'>
+            <h1>👋 Hello! My name is Oat.</h1>
+            <p>[ yes, like oatmeal. Not my real name but everyone calls me Oat ]</p>
+            <p>
+              Thanks for visiting my website! I am a frontend engineer from Thailand 🇹🇭 living in Toronto, Canada 🇨🇦
+            </p>
+            <p>I enjoy dancing, drawing and finding good deals on Facebook marketplace 🕵️</p>
+            <p>I'm also interested in self-development, Buddhism and staying active 💪</p>
+          </div>
+
+        </div>
+        <img className='profile-pic' src='https://firebasestorage.googleapis.com/v0/b/website-1057b.appspot.com/o/profile.jpg?alt=media&token=d955ead2-36e1-4a14-b522-0bf49d5bb6d0' alt='profile photo'></img>
+      </div>
+
+    </div >
   );
 }
 
